68d823ad5d85e14a116afca37a3d48502f35aec2
Carbon <yupeng08@gmail.com>
2023-03-31 22:45:49 +0530

    Update app.ts
    又改了些数字

--- app/store/app.ts
+++ app/store/app.ts
@@ -134,7 +134,7 @@ const DEFAULT_CONFIG: ChatConfig = {
   modelConfig: {
     model: "gpt-3.5-turbo",
     temperature: 1,
-    max_tokens: 2000,
+    max_tokens: 4000,
     presence_penalty: 0,
   },
 };
